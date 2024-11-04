b451cde011504d41b178f1141e91e06578365427
irBabak <babakoveisi1375@gmail.com>
2024-11-04 15:39:05 +0330

    update database commands - Babak Oveisi

--- src/mft/model/repository/database.sql
+++ src/mft/model/repository/database.sql
@@ -2,9 +2,10 @@ CREATE SEQUENCE person_seq START WITH 1 INCREMENT BY 1;
 
 CREATE TABLE persons
 (
-    personId   number PRIMARY KEY,
-    first_name nvarchar2(30),
-    last_name  nvarchar2(30)
+    personId      number PRIMARY KEY,
+    first_name    nvarchar2(30),
+    last_name     nvarchar2(30),
+    national_code nchar(10)
 );
 
 CREATE SEQUENCE simcard_seq START WITH 1 INCREMENT BY 1;
@@ -12,7 +13,7 @@ CREATE SEQUENCE simcard_seq START WITH 1 INCREMENT BY 1;
 CREATE TABLE simcards
 (
     simcardId      number PRIMARY KEY,
-    simcard_number varchar2(11),
+    simcard_number nvarchar2(11),
     ownerId        number REFERENCES persons
 );
 
@@ -21,7 +22,8 @@ SELECT simcards.simcardId      AS simcard_id,
        simcards.simcard_number AS phone_number,
        persons.personId        AS person_id,
        persons.first_name,
-       persons.last_name
+       persons.last_name,
+       persons.national_code   AS national_code
 FROM simcards
          JOIN persons
               ON simcards.ownerId = persons.personId;
