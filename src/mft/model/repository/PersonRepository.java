90bff638fbb38ed414129222245365b5fc6f3a4c
irBabak <babakoveisi1375@gmail.com>
2024-11-04 18:22:21 +0330

    add PersonRepository and SimCardRepository - Babak Oveisi

---
+++ src/mft/model/repository/PersonRepository.java
@@ -0,0 +1,122 @@
+package mft.model.repository;
+
+import mft.model.entity.Person;
+import mft.model.entity.SimCard;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PersonRepository implements AutoCloseable{
+    private Connection connection;
+    private PreparedStatement preparedStatement;
+
+    public void save(Person person) throws SQLException {
+        connection = ConnectionProvider.getConnectionProvider().getConnection();
+
+        preparedStatement = connection.prepareStatement("SELECT person_seq.nextval AS next_id FROM dual");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+        person.setId(resultSet.getInt("next_id"));
+
+        preparedStatement = connection.prepareStatement(
+                "INSERT INTO persons" +
+                        " (personId, first_name, last_name, national_code)" +
+                        " VALUES (?, ?, ?, ?)"
+        );
+        preparedStatement.setInt(1, person.getId());
+        preparedStatement.setString(2, person.getFirstName());
+        preparedStatement.setString(3, person.getLastName());
+        preparedStatement.setString(4, person.getNationalCode());
+        preparedStatement.execute();
+    }
+
+    public void edit(Person person) throws SQLException {
+        connection = ConnectionProvider.getConnectionProvider().getConnection();
+        preparedStatement = connection.prepareStatement(
+                "UPDATE persons" +
+                        " SET personId = ?, first_name = ?, last_name = ?, national_code = ?"
+        );
+        preparedStatement.setInt(1, person.getId());
+        preparedStatement.setString(2, person.getFirstName());
+        preparedStatement.setString(3, person.getLastName());
+        preparedStatement.setString(4, person.getNationalCode());
+        preparedStatement.execute();
+    }
+
+    public void remove(int id) throws SQLException {
+        connection = ConnectionProvider.getConnectionProvider().getConnection();
+        preparedStatement = connection.prepareStatement(
+                "DELETE FROM persons WHERE personId = ?"
+        );
+        preparedStatement.setInt(1, id);
+        preparedStatement.execute();
+    }
+
+    public List<Person> findAll() throws SQLException {
+        connection = ConnectionProvider.getConnectionProvider().getConnection();
+        preparedStatement = connection.prepareStatement(
+                "SELECT * FROM persons"
+        );
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        List<Person> personList = new ArrayList<>();
+        while (resultSet.next()) {
+            Person person =
+                    Person
+                            .builder()
+                            .id(resultSet.getInt("personId"))
+                            .firstName(resultSet.getString("first_name"))
+                            .lastName(resultSet.getString("last_name"))
+                            .nationalCode(resultSet.getString("national_code"))
+                            .build();
+
+            personList.add(person);
+        }
+
+        return personList;
+    }
+
+    public Person findByNationalCode(String nationalCode) throws Exception {
+        connection = ConnectionProvider.getConnectionProvider().getConnection();
+        preparedStatement = connection.prepareStatement(
+                "SELECT * FROM PERSON_SIMCARD_VIEW WHERE national_code = ?"
+        );
+        preparedStatement.setString(1, nationalCode);
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        Person person = null;
+        while (resultSet.next()) {
+            if (person == null) {
+                person =
+                        Person
+                                .builder()
+                                .id(resultSet.getInt("person_id"))
+                                .firstName(resultSet.getString("first_name"))
+                                .lastName(resultSet.getString("last_name"))
+                                .nationalCode(resultSet.getString("national_code"))
+                                .build();
+            }
+
+            SimCard simCard =
+                    SimCard
+                            .builder()
+                            .id(resultSet.getInt("simCard_id"))
+                            .simCardNumber(resultSet.getString("phone_number"))
+                            .build();
+
+            person.addSimCard(simCard);
+        }
+
+        return person;
+    }
+
+    @Override
+    public void close() throws Exception {
+        preparedStatement.close();
+        connection.close();
+    }
+}
---
+++ src/mft/model/repository/SimCardRepository.java
@@ -0,0 +1,140 @@
+package mft.model.repository;
+
+import mft.model.entity.Person;
+import mft.model.entity.SimCard;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SimCardRepository implements AutoCloseable{
+    Connection connection;
+    PreparedStatement preparedStatement;
+
+    public void save(SimCard simCard) throws SQLException {
+        connection = ConnectionProvider.getConnectionProvider().getConnection();
+        preparedStatement = connection.prepareStatement(
+                "SELECT simcard_seq.netxval AS next_id FROM dual"
+        );
+        ResultSet resultSet = preparedStatement.executeQuery();
+        simCard.setId(resultSet.getInt("next_id"));
+
+        preparedStatement = connection.prepareStatement(
+                "INSERT INTO simcards" +
+                        " (simcardId, simcard_number, ownerId)" +
+                        " VALUES (?, ?, ?)"
+        );
+        preparedStatement.setInt(1, simCard.getId());
+        preparedStatement.setString(2, simCard.getSimCardNumber());
+        preparedStatement.setInt(3, simCard.getOwner().getId());
+        preparedStatement.execute();
+    }
+
+    public void edit(SimCard simCard) throws SQLException {
+        connection = ConnectionProvider.getConnectionProvider().getConnection();
+        preparedStatement = connection.prepareStatement(
+                "UPDATE simcards" +
+                        " SET simcardId = ?, simcard_number = ?, ownerId = ?"
+        );
+        preparedStatement.setInt(1, simCard.getId());
+        preparedStatement.setString(2, simCard.getSimCardNumber());
+        preparedStatement.setInt(3, simCard.getOwner().getId());
+        preparedStatement.execute();
+    }
+
+    public void remove(int id) throws SQLException {
+        connection = ConnectionProvider.getConnectionProvider().getConnection();
+        preparedStatement = connection.prepareStatement(
+                "DELETE FROM simcards WHERE id = ?"
+        );
+        preparedStatement.setInt(1, id);
+        preparedStatement.execute();
+    }
+
+    public List<SimCard> findAll() throws SQLException {
+        connection = ConnectionProvider.getConnectionProvider().getConnection();
+        preparedStatement = connection.prepareStatement(
+                "SELECT * FROM PERSON_SIMCARD_VIEW"
+        );
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        List<SimCard> simCardList = new ArrayList<>();
+        while (resultSet.next()) {
+            Person person =
+                    Person
+                            .builder()
+                            .id(resultSet.getInt("person_id"))
+                            .firstName(resultSet.getString("first_name"))
+                            .lastName(resultSet.getString("last_name"))
+                            .nationalCode(resultSet.getString("national_code"))
+                            .build();
+
+            SimCard simCard =
+                    SimCard
+                            .builder()
+                            .id(resultSet.getInt("simcard_id"))
+                            .simCardNumber(resultSet.getString("simcard_number"))
+                            .owner(person)
+                            .build();
+
+            simCardList.add(simCard);
+        }
+
+        return simCardList;
+    }
+
+    public List<SimCard> findByPersonNationalCode(String nationalCode) throws SQLException {
+        connection = ConnectionProvider.getConnectionProvider().getConnection();
+        preparedStatement = connection.prepareStatement(
+                "SELECT * FROM PERSON_SIMCARD_VIEW WHERE national_code = ?"
+        );
+        preparedStatement.setString(1, nationalCode);
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        List<SimCard> simCardList = new ArrayList<>();
+        while (resultSet.next()) {
+            Person person =
+                    Person
+                            .builder()
+                            .id(resultSet.getInt("person_id"))
+                            .firstName(resultSet.getString("first_name"))
+                            .lastName(resultSet.getString("last_name"))
+                            .nationalCode(resultSet.getString("national_code"))
+                            .build();
+
+            SimCard simCard =
+                    SimCard
+                            .builder()
+                            .id(resultSet.getInt("simcard_id"))
+                            .simCardNumber(resultSet.getString("simcard_number"))
+                            .owner(person)
+                            .build();
+
+            simCardList.add(simCard);
+        }
+
+        return simCardList;
+    }
+
+    public int countPersonSimCardsByNationalCode(String nationalCode) throws SQLException {
+        connection = ConnectionProvider.getConnectionProvider().getConnection();
+        preparedStatement = connection.prepareStatement(
+                "SELECT COUNT(*) AS simcard_count FROM PERSON_SIMCARD_VIEW WHERE national_code = ?"
+        );
+        preparedStatement.setString(1, nationalCode);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+        return resultSet.getInt("simcard_count");
+
+    }
+
+    @Override
+    public void close() throws Exception {
+        preparedStatement.close();
+        connection.close();
+
+    }
+}
